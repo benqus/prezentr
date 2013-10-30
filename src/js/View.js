b6c9906c79bae16d8fca419480b4d872498b8806
Bence <kormos.bence@gmail.com>
2013-10-30 22:04:44 +0000

    Add DOM renderer methods

--- src/js/View.js
+++ src/js/View.js
@@ -82,6 +82,75 @@ var View = prezentr.View = Component.extend({
   },
 
   /**
+   * Appends the given Element to the View's Element or the one specified by the selector.
+   * @param element {Element}
+   * @param selector {String}
+   * @returns {View}
+   */
+  append: function (element, selector) {
+    var root = (this.find(selector) || this.element);
+
+    root.appendChild(element);
+
+    return this;
+  },
+
+  /**
+   * Appends the Element as a first child to the one specified by the provided selector.
+   * If the container Element is empty, it will append it to it.
+   * @param element {Element}
+   * @param selector {String}
+   * @returns {View}
+   */
+  prepend: function (element, selector) {
+    var first = (this.find(selector) || this.element).firstChild;
+
+    if (first && element) {
+      first.insertBefore(element);
+    } else {
+      this.append(element, selector);
+    }
+
+    return this;
+  },
+
+  /**
+   *
+   * Appends the element before the one specified by the selector.
+   * @param element {Element}
+   * @param selector {String}
+   * @returns {View}
+   */
+  before: function (element, selector) {
+    var before = this.find(selector);
+
+    if (before && element) {
+      before.insertBefore(element);
+    }
+
+    return this;
+  },
+
+  /**
+   * Appends the element after the one specified by the selector.
+   * @param element {Element}
+   * @param selector {String}
+   * @returns {View}
+   */
+  after: function (element, selector) {
+    var after = this.find(selector);
+    var before = (after || {}).nextSibling;
+
+    if (before) {
+      before.insertBefore(element);
+    } else if (after && after.parentNode) { //selector points to last child
+      after.parentNode.appendChild(element);
+    }
+
+    return this;
+  },
+
+  /**
    * Removes the View's root DOM Element form the UI.
    * @returns {View}
    */
